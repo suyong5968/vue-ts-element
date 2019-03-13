c78fe6a9c945b4a6052909cbf7ea56a50e4fc123
苏勇 <suyong@126.com>
2019-03-13 11:02:18 +0800

    创建项目

---
+++ src/views/login/login.vue
@@ -0,0 +1,15 @@
+<!--  -->
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import {Vue} from 'vue-pro'
+export default login extends Vue({
+    
+})
+</script>
+
+
+<style lang='scss' scoped>
+</style>
